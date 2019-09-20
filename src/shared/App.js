fd2ff1da6a5d88da1f5e07753a0957c1bd16ece6
jys <jayseo916@gmail.com>
2019-09-20 20:19:00 +0900

    selectType 에러 수정

--- src/shared/App.js
+++ src/shared/App.js
@@ -3,7 +3,6 @@ import { Route, Switch } from "react-router-dom";
 import Main from "../pages/Main";
 import SelectGenre from "../components/SelectGenre";
 import SelectTheme from "../components/SelectTheme";
-import SelectProblemType from "../components/SelectProblemType";
 import CreateProblem from "../components/CreateProblem";
 import MyProblem from "../pages/MyProblem";
 import MySolved from "../pages/MySolved";
@@ -19,7 +18,6 @@ class App extends React.Component {
           <Route path="/problem/main" component={CreateProblem} />
           <Route path="/1" component={SelectGenre} />
           <Route path="/2" component={SelectTheme} />
-          <Route path="/3" component={SelectProblemType} />
           <Route path="/4" component={CreateProblem} />
           <Route path="/5" component={MyProblem} />
           <Route path="/6" component={MySolved} />
