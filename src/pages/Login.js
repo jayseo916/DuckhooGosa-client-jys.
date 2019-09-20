aa51f14ba6a8b1a27df7e868f45d5450d715487b
Kang Taehoon <jadetypehoon@gmail.com>
2019-09-20 20:59:43 +0900

    Implement google login And save data at app.js state and add example for router props

---
+++ src/pages/Login.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {Redirect} from "react-router-dom";
+import {GoogleLogin} from "react-google-login";
+import "bootstrap/dist/css/bootstrap.css";
+import "../shared/App.css";
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: undefined
+    };
+  }
+
+  componentDidMount() {
+  }
+
+  responseGoogle = res => {
+    let data = {
+      email: res.profileObj.email,
+      expires_at: res.tokenObj.expires_at+ res.tokenObj.expires_in
+    };
+    this.props.setUserInfo(data);
+    localStorage.setItem("loginToken", JSON.stringify(res));
+    this.props.history.push("/main");
+  };
+  responseFail = err => {
+  };
+
+  render() {
+    return (
+        <div className="Login-page">
+          <h1> 로그인 </h1>
+          <GoogleLogin
+              clientId={process.env.REACT_APP_Google}
+              buttonText="Google"
+              onSuccess={this.responseGoogle}
+              onFailure={this.responseFail}
+          />
+        </div>
+    );
+  }
+}
+
+export default Login;
--- src/shared/App.js
+++ src/shared/App.js
@@ -3,14 +3,29 @@ import { Route, Switch } from "react-router-dom";
 import Main from "../pages/Main";
 import SelectGenre from "../components/SelectGenre";
 import SelectTheme from "../components/SelectTheme";
-import SelectProblemType from "../components/SelectProblemType";
 import CreateProblem from "../components/CreateProblem";
 import MyProblem from "../pages/MyProblem";
 import MySolved from "../pages/MySolved";
 import Loading from "../pages/Loading";
+import Login from "../pages/Login";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(prop) {
+    super(prop);
+    this.state = {
+      email: null,
+      expires_at: null
+    };
+  }
+
+  setUserInfo = data => {
+    this.setState({ email: data.email, expires_at: data.expires_at }, () => {
+      let { email, expires_at } = this.state;
+      console.log(`${email}님 로그인 ${expires_at}에 토큰 만료`);
+    });
+  };
+
   render() {
     return (
       <div className="App">
@@ -19,11 +34,16 @@ class App extends React.Component {
           <Route path="/problem/main" component={CreateProblem} />
           <Route path="/1" component={SelectGenre} />
           <Route path="/2" component={SelectTheme} />
-          <Route path="/3" component={SelectProblemType} />
           <Route path="/4" component={CreateProblem} />
           <Route path="/5" component={MyProblem} />
           <Route path="/6" component={MySolved} />
-          <Route path="/main" component={Main} />
+          <Route path="/main" render={props => <Main {...props} />} />
+          <Route
+            path="/login"
+            render={props => (
+              <Login {...props} setUserInfo={this.setUserInfo} />
+            )}
+          />
         </Switch>
       </div>
     );
