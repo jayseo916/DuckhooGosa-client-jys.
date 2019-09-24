99e633891e635183692c6fa9c5b5d035b80366dd
jys <jayseo916@gmail.com>
2019-09-24 11:28:53 +0900

    동영상까지 완성

--- src/pages/SolvingProblem.js
+++ src/pages/SolvingProblem.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "../../node_modules/video-react/dist/video-react.css";
+import problems from "./fake";
 import {
   Player,
   ControlBar,
@@ -18,42 +19,55 @@ export default class SolvingProblem extends Component {
     this.state = {
       problemId: this.props.match.params.id,
       titleImg: "",
-      backImg: ""
+      backImg: "",
+      problems: problems
     };
   }
 
+  componentDidMount() {
+    // this.loadProblem();
+  }
+
   loadProblem = async () => {
-    const problem = await axios.get(
-      `http://localhost:8000/problem/${this.state.problemId}`
-    );
-    this.setState({
-      titleImg: problem.img,
-      backImg: problem.title
+    try {
+      const problem = await axios.get(
+        `http://localhost:8000/problem/${this.state.problemId}`
+      );
+      this.setState({
+        titleImg: problem.img,
+        backImg: problem.title,
+        problems: problem.problems
+      });
+    } catch (ex) {
+      console.error(ex);
+    }
+  };
+  viewProblem = () => {
+    const { problems } = this.state.problems;
+    let view = problems.map((problem, num) => {
+      return (
+        <div key={num}>
+          <label>{num + 1}번.</label>
+          <Player>
+            <source src={`${problem.filLink1}`} />
+            <ControlBar>
+              <ReplayControl seconds={10} order={1.1} />
+              <ForwardControl seconds={30} order={1.2} />
+              <CurrentTimeDisplay order={4.1} />
+              <TimeDivider order={4.2} />
+              <PlaybackRateMenuButton rates={[5, 2, 1, 0.5, 0.1]} order={7.1} />
+              <VolumeMenuButton disabled />
+            </ControlBar>
+          </Player>
+          <p>{problem.problemText}</p>
+        </div>
+      );
     });
+
+    return view;
   };
 
   render() {
-    return (
-      <div>
-        {/* <img src="https://duckhoogosa.s3.ap-northeast-2.amazonaws.com/practice01/Screen+Shot+2019-09-19+at+1.26.59+PM.png" /> */}
-        <Player
-          playsInline
-          poster="https://duckhoogosa.s3.ap-northeast-2.amazonaws.com/practice01/Screen+Shot+2019-09-19+at+1.26.59+PM.png"
-          src="https://duckhoogosa.s3.ap-northeast-2.amazonaws.com/practice01/MIKURU_LEGEND.mp4"
-        />
-        <Player poster="https://duckhoogosa.s3.ap-northeast-2.amazonaws.com/practice01/Screen+Shot+2019-09-19+at+1.26.59+PM.png">
-          <source src="https://duckhoogosa.s3.ap-northeast-2.amazonaws.com/practice01/MIKURU_LEGEND.mp4" />
-
-          <ControlBar>
-            <ReplayControl seconds={10} order={1.1} />
-            <ForwardControl seconds={30} order={1.2} />
-            <CurrentTimeDisplay order={4.1} />
-            <TimeDivider order={4.2} />
-            <PlaybackRateMenuButton rates={[5, 2, 1, 0.5, 0.1]} order={7.1} />
-            <VolumeMenuButton disabled />
-          </ControlBar>
-        </Player>
-      </div>
-    );
+    return <div>{this.viewProblem()}</div>;
   }
 }
