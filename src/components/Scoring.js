0727ad01bee41d7b1ecd87e6df8feafd95bd271f
ggsuccess <ggsuccess@naver.com>
2019-09-27 15:30:31 +0900

    eval..ing.

--- src/components/Scoring.js
+++ src/components/Scoring.js
@@ -1,11 +1,43 @@
 import React, { Component } from "react";
 import "../../node_modules/nes.css/css/nes.css";
-
+import { Modal } from "antd";
+import { config } from "../config";
+import axios from "axios";
 export class Scoring extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      visible: true,
+      evalQ:"",
+      evalD:"",
+      comment:""
+    }
+  }
+  
   goComment = () => {
     this.props.history.push(`/commnet/${this.props.data.problemId}`);
   };
+  componentDidMount() {
 
+  }
+  evalSubmit(prop, evalQual, evalDiff, comment) {
+    alert(JSON.stringify(prop));
+    this.setState({ visible: false })
+    axios.post(`http://localhost:8000/problem/evaluation`,{
+      _id: prop, 
+      evalQ: evalQual,
+      evalD: evalDiff,
+      comments: comment
+    }, config)
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+  }
+  cancel(e) {
+    this.setState({visible: false})
+  }
+  commentHandle(e) {
+    this.setState({comment:e.target.value.trim()})
+  }
   viewScoring = checkProblem => {
     //문제 오브젝트어레이 넣으면 맞춘문제 틀린문제 표시해서 보여줌
     let viewProblem = checkProblem.map(v => {
@@ -53,7 +85,18 @@ export class Scoring extends Component {
       }
     });
     let viewProblem = this.viewScoring(checkProblem); //만춘문제 틀린문제 뷰
+    const { evalQ, evalD, comment } = this.state;
     return (
+    <div>
+      <Modal 
+        title="해당 문제 평가" 
+        visible={this.state.visible} 
+        onOk={() => this.evalSubmit(problemId, evalQ, evalD, comment)} 
+        onCancel={this.cancel}>
+        <div>문제가 어땠나요?(좋았어요:5점,구렸어요:1점)</div>
+        <div>문제가 어려웠나요?(어려웠어요:5점,쉬웠어요:1점)</div>
+        의견:<input type="text" onChange={e => this.commentHandle(e)}></input>
+      </Modal>
       <div style={{ padding: "0 0 60px 30px" }}>
         <div className="nes-container with-title is-centered">
           <h1 className="title">SCORE!!</h1>
@@ -87,6 +130,7 @@ export class Scoring extends Component {
           COMMENT [{commentCount}]
         </button>
       </div>
+    </div>
     );
   }
 }
