c2784521c95f56415c9ba6415f418347cfb9bd5d
ggsuccess <ggsuccess@naver.com>
2019-10-05 13:23:17 +0900

    commentComplete

--- src/pages/Comment.js
+++ src/pages/Comment.js
@@ -8,17 +8,11 @@ export default class Comment extends React.Component {
     super(props);
     this.state = {
       problem_id: this.props.match.params.id,
-      comments: [
-        {
-          comment: null,
-          nick: null,
-          day: null
-        }
-      ]
+      comments: []
     };
   }
   componentDidMount() {
-    // console.log("scoring에서 넘어온 props값:" + JSON.stringify(this.props));
+    // console.log(this.props);
     // let temp;
     axiosInstance
       .get(`/comment/${this.state.problem_id}`, config)
@@ -30,32 +24,23 @@ export default class Comment extends React.Component {
   }
   render() {
     const { comments } = this.state;
-    console.log(comments);
-    if (comments[0].comment !== null) {
-      comments.map((data, i) => {
-        return (
-          <div key={i} className="nes-container is-rounded">
-            <div>
-              {data.nick}님의 의견:
-              <div style={{ fontSize: "20px" }}>{data.comment}</div>
-            </div>
-            날짜:{data.day}
+    // console.log(comments);
+    if (comments) {
+      const list = comments.map((data, i) => (
+        <div key={i} className="nes-container is-rounded">
+          <div>
+            <span style={{ fontStyle: "Italic", color: "blue" }}>
+              {!data.nick ? "익명의 더쿠" : data.nick}
+            </span>
+            님의 의견:
+            <div style={{ fontSize: "20px" }}>{data.comment}</div>
           </div>
-        );
-      });
+          날짜:{data.day}
+        </div>
+      ));
+      return <div>의견 모음{list}</div>;
     } else {
       return <div>아직 해당문제에 대한 의견이 없습니다.</div>;
     }
-    // return comments.map((data, i) => (
-    //   <div key={i} className="nes-container is-rounded">
-    //     <div>
-    //       {data.nick}님의 의견:
-    //       <div style={{ fontSize: "20px" }}>{data.comment}</div>
-    //     </div>
-    //     날짜:{data.day}
-    //     <hr></hr>
-    //   </div>
-    // ));
-    // return <div>ddd</div>;
   }
 }
