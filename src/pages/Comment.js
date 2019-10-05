1f172de0eba1fe58a2a66e934a0b7638e76b23d9
ggsuccess <ggsuccess@naver.com>
2019-10-05 20:47:02 +0900

    merged

--- src/pages/Comment.js
+++ src/pages/Comment.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import axiosInstance from "axios";
 import { axiosInstance, config } from "../config";
 import "../shared/App.css";
 
@@ -7,12 +6,14 @@ export default class Comment extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      commentBtn: false,
+      inputComment: "",
       problem_id: this.props.match.params.id,
       comments: []
     };
   }
   componentDidMount() {
-    // console.log(this.props);
+    console.log(this.props);
     // let temp;
     axiosInstance
       .get(`/comment/${this.state.problem_id}`, config)
@@ -24,6 +25,7 @@ export default class Comment extends React.Component {
   }
   render() {
     const { comments } = this.state;
+    const commentBtn = this.state.commentBtn;
     // console.log(comments);
     if (comments) {
       const list = comments.map((data, i) => (
@@ -38,9 +40,53 @@ export default class Comment extends React.Component {
           날짜:{data.day}
         </div>
       ));
-      return <div>의견 모음{list}</div>;
+      return (
+        <div>
+          {!commentBtn ? (
+            <div>
+              <textarea
+                cols="100"
+                rows="4"
+                placeholder="의견을 남겨주세요"
+              ></textarea>
+              <button onClick={() => this.submitComment()}>댓글 쓰기</button>
+              <button onClick={() => this.commentBtnHandle()}>댓글버튼</button>
+            </div>
+          ) : (
+            <div>
+              <button onClick={() => this.commentBtnHandle()}>
+                댓글입력창없애기
+              </button>
+            </div>
+          )}
+          <div>의견 모음{list}</div>
+        </div>
+      );
     } else {
-      return <div>아직 해당문제에 대한 의견이 없습니다.</div>;
+      return (
+        <div>
+          <div>아직 해당문제에 대한 의견이 없습니다.</div>
+          {!commentBtn ? (
+            <div>
+              <textarea
+                cols="100"
+                rows="4"
+                placeholder="의견을 남겨주세요"
+              ></textarea>
+              <button onClick={() => this.submitComment()}>댓글 쓰기</button>
+              <button onClick={() => this.commentBtnHandle()}>
+                댓글입력창
+              </button>
+            </div>
+          ) : (
+            <div>
+              <button onClick={() => this.commentBtnHandle()}>
+                댓글입력창없애기
+              </button>
+            </div>
+          )}
+        </div>
+      );
     }
   }
 }
